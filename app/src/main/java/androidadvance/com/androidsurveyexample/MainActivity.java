fbd61d6008d536cc45409ac5dbffc0981604e946
Han Liang Wee Eric <eric.aka.darth_vader@hotmail.com>
2018-07-03 15:36:22 +0800

    Added sample JSON for debug purposes

--- app/src/main/java/androidadvance/com/androidsurveyexample/MainActivity.java
+++ app/src/main/java/androidadvance/com/androidsurveyexample/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
 import com.e2empire.survey.SurveyActivity;
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,40 +21,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Nothing fancy here. Plain old simple buttons....
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.btn_container);
 
-        Button button_survey_example_1 = (Button) findViewById(R.id.button_survey_example_1);
-        Button button_survey_example_2 = (Button) findViewById(R.id.button_survey_example_2);
-        Button button_survey_example_3 = (Button) findViewById(R.id.button_survey_example_3);
-
-        button_survey_example_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent i_survey = new Intent(MainActivity.this, SurveyActivity.class);
-                //you have to pass as an extra the json string.
-                i_survey.putExtra("json_survey", loadSurveyJson("example_survey_1.json"));
-                startActivityForResult(i_survey, SURVEY_REQUEST);
+        final String samplesPath = "samples";
+        try {
+            String[] questionnairePaths = getAssets().list(samplesPath);
+            for(final String eaQuestionnairePath:questionnairePaths) {
+                Button btn = new Button(this);
+                btn.setText(eaQuestionnairePath);
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i_survey = new Intent(MainActivity.this, SurveyActivity.class);
+                        i_survey.putExtra("json_survey", loadSurveyJson(samplesPath + "/" + eaQuestionnairePath));
+                        startActivityForResult(i_survey, SURVEY_REQUEST);
+                    }
+                });
+                linearLayout.addView(btn);
             }
-        });
-
-        button_survey_example_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i_survey = new Intent(MainActivity.this, SurveyActivity.class);
-                i_survey.putExtra("json_survey", loadSurveyJson("example_survey_2.json"));
-                startActivityForResult(i_survey, SURVEY_REQUEST);
-            }
-        });
-
-        button_survey_example_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i_survey = new Intent(MainActivity.this, SurveyActivity.class);
-                i_survey.putExtra("json_survey", loadSurveyJson("example_survey_3.json"));
-                startActivityForResult(i_survey, SURVEY_REQUEST);
-            }
-        });
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
 
     }
 
@@ -87,6 +76,4 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
-
-
 }
